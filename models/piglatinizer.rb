e86a9bf95c715548c5ea39a88a5884224f46eb49
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-20 00:43:30 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,15 +1,15 @@
 require 'pry'
 class PigLatinizer
   
-   def piglatinize(user_phrase)
-    if user_phrase.split(" ").length == 1 
-      piglatinize(user_phrase)
-    else 
-      to_pig_latin(user_phrase)
-    end 
-   end 
+  # def piglatinize(user_phrase)
+  #   if user_phrase.split(" ").length == 1 
+  #     piglatinize(user_phrase)
+  #   else 
+  #     to_pig_latin(user_phrase)
+  #   end 
+  # end 
 
-  def piglatinize_word(user_phrase)
+  def piglatinize(user_phrase)
     @new_word = user_phrase.split("")
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     if vowels.include?(@new_word[0])
